08ebbbf123407b479e8159d59908a3761a80d55b
bekov0004 <116736021+bekov0004@users.noreply.github.com>
2022-12-17 15:38:43 +0300

    Practice-and-homework-16.12.22 15:38

--- Сalculator/CalculatorMath.cs
+++ Сalculator/CalculatorMath.cs
@@ -5,35 +5,90 @@ public const double PI = 3.14;
 public const double E = 2.71;
 public double Abs(double value)
 {
-    return Math.Abs(value);
+    if(value<0)
+    {
+        return value*-1;
+    }
+    else
+    {
+        return value;
+    }
 }
 public float Abs(float value)
 {
-    return Math.Abs(value);
+    if(value<0)
+    {
+        return value*-1;
+    }
+    else
+    {
+        return value;
+    }
 }
 public short Abs(short value)
 {
-    return Math.Abs(value);
+       if(value<0)
+    {
+        return (short)(value*-1);
+    }
+    else
+    {
+        return (short)value;
+    }
 }
 public int Abs(int value)
 {
-    return Math.Abs(value);
+        if(value<0)
+    {
+        return value*-1;
+    }
+    else
+    {
+        return value;
+    }
 }
 public double Pow(double x, double y)
 {
-    return Math.Pow(x,y);
+    double cnt =1;
+    for (int i = 0; i < y; i++)
+    {
+        cnt=cnt*x;
+    }
+    return cnt;
 }
-public double Sqrt(double d)
+public int Sqrt(int d)
 {
-    return Math.Sqrt(d);
+    int a=0;
+    for (int i = 0; i < d; i++)
+    {
+        if (i*i==d)
+        {
+            a = i;
+        }
+    }
+    return a;
 }
 public int Max(int val1, int val2)
 {
-    return Math.Max(val1,val2);
+    if (val1>val2)
+    {
+        return val1;
+    }
+    else
+    {
+        return val2;
+    }
 }
 public int Min(int val1, int val2)
 {
-    return Math.Min(val1,val2);
+    if (val1<val2)
+    {
+        return val1;
+    }
+    else
+    {
+        return val2;
+    }
 }
  
 
--- Сalculator/Program.cs
+++ Сalculator/Program.cs
@@ -2,5 +2,5 @@
 int n,k;
 n =  Convert.ToInt32(Console.ReadLine());
 k =  Convert.ToInt32(Console.ReadLine());
-System.Console.WriteLine($"Max = {Calcul.Max(n,k)}");
+System.Console.WriteLine($"Max = {Calcul.Sqrt(n)}");
 System.Console.WriteLine($"Min = {Calcul.Min(n,k)}");
